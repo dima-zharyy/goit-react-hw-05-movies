58d0684086d09ffbcfd445ed9ab97fdcafdf2ffd
dima-zharyy <zharyydimka@gmail.com>
2022-08-13 11:46:55 +0300

    added navigate if fetch returns reject

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,18 +1,22 @@
 import { Details, Loader } from 'components';
 import { useState, useEffect, Suspense } from 'react';
-import { useParams, Outlet } from 'react-router-dom';
+import { useParams, Outlet, useNavigate } from 'react-router-dom';
 import { getDetails } from 'service';
 import { Container } from './MovieDetails.styled';
 
 export const MovieDetails = () => {
   const [movieDetails, setMovieDetails] = useState(null);
   const { movieId } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     getDetails(movieId)
       .then(setMovieDetails)
-      .catch(error => console.log(error.message));
-  }, [movieId]);
+      .catch(error => {
+        navigate('/', { replace: true });
+        console.log(error.message);
+      });
+  }, [movieId, navigate]);
 
   return (
     <main>
